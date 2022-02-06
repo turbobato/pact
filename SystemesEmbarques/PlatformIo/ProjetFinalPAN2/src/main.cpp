6b27195e694acaa5eea439b67f277cf3c9376892
dariocarvalho <dario.carvalho@telecom-paris.fr>
2022-02-06 15:41:31 +0100

    Version avec led Eclairage

--- SystemesEmbarques/PlatformIo/ProjetFinalPAN2/src/main.cpp
+++ SystemesEmbarques/PlatformIo/ProjetFinalPAN2/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
-#include<DFRobot_SHT20.h>         //BIBLIOTHEQUE CAPTEUR SHT20 (TEMPERATURE ET HUMIDIYE DE L'AIR)
-#include "DFRobot_VEML7700.h"     // BIBLIOTHEQUE CAPTEUR DE LUMINOSITE
+//BIBLIOTHEQUE CAPTEUR SHT20 (TEMPERATURE ET HUMIDIYE DE L'AIR)
+#include<DFRobot_SHT20.h>    
+// BIBLIOTHEQUE CAPTEUR DE LUMINOSITE     
+#include "DFRobot_VEML7700.h"     
+// BIBLIOTHEQUE LEDS
+#include <Adafruit_NeoPixel.h>    
 
-#include <Adafruit_NeoPixel.h>    // BIBLIOTHEQUE LEDS
+// BIBLIOTHEQUES SERVEUR
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
+//BIBLIOTHEQUE JSON
+#include<ArduinoJson.h>
+
+
+//Variable de synchronisation
+
+float humiditySol=0;
+float humidityAir=0;
+float temperature=0;
+float luminosity=0;
+
+int red = 0;
 
 
 /****************************************************************
@@ -26,14 +44,14 @@ void ConfigLED(){
 ***************************************************************/
 
 
-void controlLed(int red, int green, int blue, int frequence)
+void controlLed(int led,int red, int green, int blue)
 { 
-    pixels.setPixelColor(0, pixels.Color(red,green,blue)); 
+    pixels.setPixelColor(led, pixels.Color(red,green,blue)); 
     pixels.show(); 
-    delay(frequence);
-    pixels.setPixelColor(0, pixels.Color(0,0,0)); 
-    pixels.show(); 
-    delay(frequence); // a voir apres pour changer les delay() en timer qui ne bloque pas tout le programme, mais seulement la led
+    //delay(frequence);
+    //pixels.setPixelColor(0, pixels.Color(0,0,0)); 
+    //pixels.show(); 
+    //delay(frequence); // a voir apres pour changer les delay() en timer qui ne bloque pas tout le programme, mais seulement la led
 }
 
 
@@ -49,7 +67,7 @@ void controlLed(int red, int green, int blue, int frequence)
 
 float readSoilMoisture()
 {
-  return (analogRead(4)*100/4095);  // 0-30 (sec) 30-70 (ça va) (70-95)
+  return (analogRead(35)*100/4095);  // 0-30 (sec) 30-70 (ça va) (70-95)
 }
 
 
@@ -140,6 +158,118 @@ float ReadLuminosite(){
 *FIN DES FONCTIONS DE LECTURE DES GRANDEURS ISSUES DU CAPTEUR STH20***
 **********************************************************************/
 
+
+/**************************************************************
+*CONFIGURATION CONNEXION INTERNET*******************************
+***************************************************************/
+
+
+//MOT DE PASS ET NOM DU RESEAU
+const char* ssid = "pact51";
+const char* password =  "2022Pact51!";
+
+//Serveur sur le port 80
+AsyncWebServer server(80);
+
+
+/*********************************************************************
+*FONCTIONS CONNEXION INTERNET ****************************************
+**********************************************************************/
+
+void connectRouter(){
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.print("Connected to : ");
+  Serial.println(WiFi.localIP());
+  controlLed(1,0,100,0);
+}
+
+/*********************************************************************
+*FIN DE FONCTION CONNEXION INTERNET ***********************************
+**********************************************************************/
+
+
+/*********************************************************************
+*FONCTIONS REPONSE AUX REQUETES****************************************
+**********************************************************************/
+
+void sendMeasurements(){
+
+  //REPONSE REQUETE DEMANDE DE LA TEMPERATURE
+   server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest * request) {
+   request->send(200, "text/plain", "TEMPERATURE= "+String(temperature) + " Deg");
+  });
+  
+  //REPONSE REQUETE DEMANDE DE L'HUMIDITE DE L'AIR
+  server.on("/humidityair", HTTP_GET, [](AsyncWebServerRequest * request) {
+
+    request->send(200, "text/plain", "HUMIDITE DE L'AIR = "+String(humidityAir) + " %");
+  });
+
+  //REPONSE REQUETE DEMANDE DE L'HUMIDITÉ DU SOL
+   server.on("/humiditysoil", HTTP_GET, [](AsyncWebServerRequest * request) {
+   
+   request->send(200, "text/plain", "HUMIDITE DU SOL = "+String(humiditySol) + "%");
+  });
+
+  //REPONSE REQUETE DEMANDE DE LA LUMINOSITÉ
+   server.on("/luminosity", HTTP_GET, [](AsyncWebServerRequest * request) {
+   
+   request->send(200, "text/plain", "LUMINOSITE : = "+String(luminosity) + " Lux");
+  });
+
+  // REPONSE JSON CONTENANT LES VALEURS DE TOUTES LES GRANDEURS
+    server.on("/allstatus", HTTP_GET, [](AsyncWebServerRequest *request){
+          AsyncResponseStream *response = request->beginResponseStream("application/json");
+      DynamicJsonDocument json(1024);
+      json["status"] = "ON EST LA GROUPE 5.1";
+      json["temperature"] = temperature;
+      json["luminosité"] = luminosity;
+      json["Humidité de l'air  = "] = humidityAir;
+      json["Humidité du sol  = "] = humiditySol;
+      serializeJson(json, *response);
+      request->send(response);
+    });
+
+ 
+  server.begin();
+}
+
+
+/*********************************************************************
+*FIN DE FONCTION REPONSE AUX REQUETES**********************************
+**********************************************************************/
+
+
+/*********************************************************************
+*FONCTIONS COMMANDE DES LEDS D'ECLAIRAGE*******************************
+**********************************************************************/
+
+#define RED_PLANTE 19
+#define GREEN_PLANTE 17
+#define BLUE_PLANTE  18
+
+//CONFIGURATION DES BROCHES
+void configLedPlantes(){
+  pinMode(RED_PLANTE ,OUTPUT);
+  pinMode(GREEN_PLANTE,OUTPUT);
+  pinMode (BLUE_PLANTE, OUTPUT);
+}
+
+// COMMANDE DES BROCHES
+void controlPlantesLeds(bool redOn, bool greenOn, bool blueOn){
+  digitalWrite(RED_PLANTE, redOn);
+  digitalWrite(BLUE_PLANTE,blueOn);
+  digitalWrite(GREEN_PLANTE,greenOn);
+}
+
+/*********************************************************************
+*FIN DE FONCTION DE COMMANDE DES LEDS D'ECLAIRAGE ********************
+**********************************************************************/
+
 void setup()
 {
 //Configuration du capteur de Luminosité y compris initialization  
@@ -151,7 +281,14 @@ ConfigCapteurLum();
 
 //Initialization de la Serie avec u BAUD RATE DE 9600bit/s
 Serial.begin(9600);
+
+connectRouter();
+
+sendMeasurements();
+configLedPlantes();
 }
+
+
 
 
 
@@ -160,19 +297,36 @@ Serial.begin(9600);
 void loop()
 {
   
+  temperature = ReadTemperature();
+  humidityAir = ReadAireHumidite();
+  humiditySol = readSoilMoisture();
+  luminosity =  ReadLuminosite();
+
   
+  controlLed(0,red,100,100);
+  if (red<100) red++;
+  else red =0;
+
+  controlPlantesLeds(true,false,false);
+  delay(100);
+
+  controlPlantesLeds(false,true,false);
+  delay(100);
+  controlPlantesLeds(false,false,true);
+  delay(100);
   Serial.print("Temperature  = ");
-  Serial.print(ReadTemperature());
+  Serial.print(temperature );
   Serial.print("°C   ");
   Serial.print("Humidite l'air  = ");
-  Serial.print(ReadAireHumidite());
+  Serial.print(humidityAir);
   Serial.print("%    ");
   Serial.print("Luminosite  = ");
-  Serial.print(ReadLuminosite());
+  Serial.print(luminosity);
   Serial.print("Lux    ");
   Serial.print("Humidi Sol =    ");
-  Serial.print(readSoilMoisture());
+  Serial.print(humiditySol);
   Serial.println("%    ");
 
-
+  
+  
 }
