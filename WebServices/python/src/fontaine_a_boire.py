c986d880854c59e3c833fab65548e89de88c941b
Guilhem JAZERON <guilhem.jazeron@telecom-paris.fr>
2022-02-08 17:31:21 +0100

    appel du service de fontaines avec un script basique en ligne de commande pour chercher une fontaine en entrant une chaine de caractère

---
+++ WebServices/python/src/fontaine_a_boire.py
@@ -0,0 +1,33 @@
+import requests
+
+# script basique permettant de rechercher une fontaine en fonction d'un lieu et renvoyer sa position géographique et sa disponibilité
+
+q=input("Entrez votre requête :\n")
+
+rows=input("Entrez le nombre de résultats souhaités\n")
+
+request = "http://opendata.paris.fr/api/records/1.0/search/?dataset=fontaines-a-boire&q="+q+"&rows="+rows+"BA&facet=type_objet&facet=modele&facet=commune&facet=dispo"
+
+response = requests.get(request)
+
+results = response.json()["records"]
+
+if (results == []):
+    print("Aucune réponse ne satisfaisant votre reqûete a été trouvée")
+
+else :
+    print("Voici vos résultats")
+    j=1
+    for i in results :
+        print(str(j)+"\n")
+        j+=1
+        fields=i["fields"]
+        print("Emplacement de la fontaine :\n")
+        lieu = fields["voie"] + " à " + fields["commune"]
+        print(lieu+"\n")
+        dispo = fields["dispo"]
+        if dispo=="OUI":
+            print("La fontaine est disponible \n")
+        else : 
+            print("La fontaine n'est pas disponible jusqu'à "+fields["fin_ind"]+" pour raison de "+fields["motif_ind"]+"\n")
+        print("Fontaine de type : " + fields["type_objet"]+ "\n")
