5d7dd9efc858755cc08b08bc47705bbc962a33f9
Guilhem Jazeron <turbobato@pop-os.localdomain>
2022-03-25 11:24:27 +0100

    écriture du code pour le forum en java pour poster/récupérer des messages, le code tournera dans l'application android

--- WebServices/python/src/PAN3/app/app.py
+++ WebServices/python/src/PAN3/app/app.py
@@ -4,6 +4,8 @@ from flask import Flask, jsonify, requests
 
 app=Flask(__name__)
 
+bdd_url = "test"
+
 @app.route('/retrieve-message')
 def message():
     return jsonify(User = "Guilhem JAZERON", Message = "Salut, je vous parle")
---
+++ WebServices/python/src/PAN3/app/test_serveur.py
@@ -0,0 +1,3 @@
+import requests 
+
+print(requests.get("http://137.194.210.192:8055/items/Potagers").json())
