17b6cccf08208fa9be2a87736ae1a8df62796b4e
dariocarvalho <dario.carvalho@telecom-paris.fr>
2022-05-30 15:14:38 +0200

    suppression code time

--- SystemesEmbarques/PlatformIo/ProjetFinalPAN2/src/BoxAg.cpp
+++ SystemesEmbarques/PlatformIo/ProjetFinalPAN2/src/BoxAg.cpp
@@ -304,37 +304,6 @@ void BoxAg::timeConfig(){
 }
 
 void BoxAg::countTime(){
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-  }
-  else
-   {
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-    Serial.print("Day of week: ");
-    Serial.println(&timeinfo, "%A");
-    Serial.print("Month: ");
-    Serial.println(&timeinfo, "%B");
-    Serial.print("Day of Month: ");
-    Serial.println(&timeinfo, "%d");
-    Serial.print("Year: ");
-    Serial.println(&timeinfo, "%Y");
-    Serial.print("Hour: ");
-    Serial.println(&timeinfo, "%H");
-    Serial.print("Hour (12 hour format): ");
-    Serial.println(&timeinfo, "%I");
-    Serial.print("Minute: ");
-    Serial.println(&timeinfo, "%M");
-    Serial.print("Second: ");
-    Serial.println(&timeinfo, "%S");
-    Serial.println("Time variables");
-    char timeHour[3];
-    strftime(timeHour,3, "%H", &timeinfo);
-    Serial.println(timeHour);
-    char timeWeekDay[10];
-    strftime(timeWeekDay,10, "%A", &timeinfo);
-    Serial.println(timeWeekDay);
-    Serial.println();
-   } 
+  
   
 }
